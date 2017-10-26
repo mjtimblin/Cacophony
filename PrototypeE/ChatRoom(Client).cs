7b8824cc870a61c3c62cd01c444ce2b32e6a6035
Michael Timblin <mjtimblin@gmail.com>
2017-10-26 15:25:33 -0500

    Add code from PrototypeD to PrototypeE, and remove autogenerated Form1.

---
+++ PrototypeE/ChatRoom(Client).cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace PrototypeD
+{
+    public partial class ChatRoom_Client_ : Form
+    {
+        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+        public string IP;
+        public int port = 5764;
+        public string passwordProvided;
+
+        public ChatRoom_Client_()
+        {
+            InitializeComponent();
+        }
+
+        private void ChatRoom_Client__Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void StartClient()
+        {
+            clientSocket.Connect(IP, port);
+            Thread newThread = new Thread(ListenToServer);
+            newThread.Start();
+            Message mes = new Message("ValidateAttempt", "NameTest", passwordProvided);
+            SendToServer(mes);
+            //Password operations
+        }
+
+        private void ListenToServer()
+        {
+            while (true)
+            {
+                NetworkStream networkStream = clientSocket.GetStream();
+                byte[] bytesFrom = new byte[10025];
+                networkStream.Read(bytesFrom, 0, 10025);
+                var mes = new Message(bytesFrom);
+
+                HandleServerMessage(mes);
+            }
+        }
+
+        private void HandleServerMessage(Message message)
+        {
+            if (message.command == "ValidateFail")
+                Prompt("Incorrect Password");
+            else if (message.command == "ValidatePass")
+                Prompt("Correct Password");
+            else
+                Log(message.message);
+        }
+
+        private void SendToServer(Message message)
+        {
+            NetworkStream networkStream = clientSocket.GetStream();
+            byte[] outStream = message.ToJsonBytes();
+            networkStream.Write(outStream, 0, outStream.Length);
+            networkStream.Flush();
+            txtMessage.Text = "";
+        }
+
+        public void Log(String text)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action<string>(Log), new object[] { text });
+                return;
+            }
+            txtChat.AppendText(Environment.NewLine + text);
+        }
+
+        private void Prompt(string message)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action(() => Prompt(message)));
+                return;
+            }
+            MessageBox.Show(message);
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            SendToServer(new Message("Message", "NameTest", txtMessage.Text));
+        }
+    }
+}
---
+++ PrototypeE/StartForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace PrototypeD
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnHost = new System.Windows.Forms.Button();
+            this.btnJoin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(293, 24);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Maroon;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(270, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(20, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnHost
+            // 
+            this.btnHost.BackColor = System.Drawing.Color.Firebrick;
+            this.btnHost.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHost.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHost.Location = new System.Drawing.Point(12, 30);
+            this.btnHost.Name = "btnHost";
+            this.btnHost.Size = new System.Drawing.Size(131, 46);
+            this.btnHost.TabIndex = 1;
+            this.btnHost.Text = "Host Server";
+            this.btnHost.UseVisualStyleBackColor = false;
+            this.btnHost.Click += new System.EventHandler(this.btnHost_Click);
+            // 
+            // btnJoin
+            // 
+            this.btnJoin.BackColor = System.Drawing.Color.Firebrick;
+            this.btnJoin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnJoin.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJoin.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.btnJoin.Location = new System.Drawing.Point(149, 30);
+            this.btnJoin.Name = "btnJoin";
+            this.btnJoin.Size = new System.Drawing.Size(133, 46);
+            this.btnJoin.TabIndex = 2;
+            this.btnJoin.Text = "Join";
+            this.btnJoin.UseVisualStyleBackColor = false;
+            this.btnJoin.Click += new System.EventHandler(this.btnJoin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cacophony";
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(293, 88);
+            this.Controls.Add(this.btnJoin);
+            this.Controls.Add(this.btnHost);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "StartForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Welcome!";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.StartForm_MouseDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnHost;
+        private System.Windows.Forms.Button btnJoin;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
