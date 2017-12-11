3a2a0ad83e08140869d34c72dc8c6c33299a4cee
Michael J Timblin <timblinm@uwplatt.edu>
2017-12-10 21:46:25 -0600

    Add try catch for join group failure case.

--- Cacophony/Cacophony/Forms/JoinGroupForm.cs
+++ Cacophony/Cacophony/Forms/JoinGroupForm.cs
@@ -13,36 +13,39 @@ using System.Net.Sockets;
 
 namespace Cacophony.Forms
 {
-    public partial class JoinGroupForm : Form
-    {
-        public JoinGroupForm()
-        {
-            InitializeComponent();
-        }
+   public partial class JoinGroupForm : Form
+   {
+      public JoinGroupForm()
+      {
+         InitializeComponent();
+      }
 
-        private void btnJoinGroup_Click(object sender, EventArgs e)
-        {
-            this.Hide();//Might need to dispose of this better.
-            User user = new User(-1, txtUsername.Text, txtPIN.Text);
-            user.UserID = Login(txtIP.Text, txtPassword.Text, user);
-            if (user.UserID != -1)
-            {
-                var ClientChat = new ClientChatForm(user, txtIP.Text);
-                Thread thread = new Thread(ApplicationRunProc);
-                thread.SetApartmentState(ApartmentState.STA);
-                thread.IsBackground = true;
-                thread.Start(ClientChat);
-            }
-        }
+      private void btnJoinGroup_Click(object sender, EventArgs e)
+      {
+         this.Hide();//Might need to dispose of this better.
+         User user = new User(-1, txtUsername.Text, txtPIN.Text);
+         user.UserID = Login(txtIP.Text, txtPassword.Text, user);
+         if (user.UserID != -1)
+         {
+            var ClientChat = new ClientChatForm(user, txtIP.Text);
+            Thread thread = new Thread(ApplicationRunProc);
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.IsBackground = true;
+            thread.Start(ClientChat);
+         }
+      }
 
-        private int Login(string ip, string password, User user)
-        {
+      private int Login(string ip, string password, User user)
+      {
+         NetworkStream networkStream = null;
+         try
+         {
             var clientSocket = new System.Net.Sockets.TcpClient();
             clientSocket.Connect(ip, Server.PORT);
             AppCode.Message request = new CommandMessage(user.UserID, user.Alias, AppCode.CommandType.ValidateAttempt, password + "|" + user.Alias + "|" + user.PIN);//Might need to change format
 
             //Send password attempt
-            NetworkStream networkStream = clientSocket.GetStream();
+            networkStream = clientSocket.GetStream();
             byte[] outStream = request.SerializeMessage();
             networkStream.Write(outStream, 0, outStream.Length);
             networkStream.Flush();
@@ -50,35 +53,35 @@ namespace Cacophony.Forms
             //Listen for password accepted
             networkStream.ReadTimeout = 5000;
             byte[] bytesFrom = new byte[10025];
-            try
-            {
-                networkStream.Read(bytesFrom, 0, 10025);
-                networkStream.ReadTimeout = Timeout.Infinite;
-                var response = AppCode.Message.DeserializeMessage(bytesFrom);
-                if (response is CommandMessage && ((CommandMessage)response).type == AppCode.CommandType.ValidateConfirm)
-                {
-                    var serverResponse = ((CommandMessage)response).content.ToString().Split('|');
-                    if (serverResponse[0] == "false")
-                        MessageBox.Show("Wrong password or PIN.");
-                    else
-                        return int.Parse(serverResponse[1]);
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Server did not respond!");
-                return -1;
-            }
-            finally
-            {
-                networkStream.ReadTimeout = Timeout.Infinite;
-            }
-            return -1;
-        }
 
-        private static void ApplicationRunProc(object state)
-        {
-            Application.Run(state as Form);
-        }
-    }
+            networkStream.Read(bytesFrom, 0, 10025);
+            networkStream.ReadTimeout = Timeout.Infinite;
+            var response = AppCode.Message.DeserializeMessage(bytesFrom);
+            if (response is CommandMessage && ((CommandMessage)response).type == AppCode.CommandType.ValidateConfirm)
+            {
+               var serverResponse = ((CommandMessage)response).content.ToString().Split('|');
+               if (serverResponse[0] == "false")
+                  MessageBox.Show("Wrong password or PIN.");
+               else
+                  return int.Parse(serverResponse[1]);
+            }
+         }
+         catch (Exception ex)
+         {
+            MessageBox.Show("Server did not respond!");
+            return -1;
+         }
+         finally
+         {
+            if (networkStream != null)
+               networkStream.ReadTimeout = Timeout.Infinite;
+         }
+         return -1;
+      }
+
+      private static void ApplicationRunProc(object state)
+      {
+         Application.Run(state as Form);
+      }
+   }
 }
